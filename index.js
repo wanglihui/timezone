5de978961327e3d147dd67db0dc2bfee8be2f1ee
wanglihui <lihui.wang@tulingdao.com>
2016-12-22 11:15:07 +0800

    直接提供getTimezoneDate function

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-var timezone = require("./timezone");
+'use strict';
+
+var timezone ={};
+var data = require("./timezone");
+
+timezone.getTimezoneDate = function(datestr, citycode) {
+  if (!data[citycode]) {
+    throw new Error('unSupport city code')
+  }
+  let offsetUtc = data[citycode][3];
+  if (typeof offsetUtc == 'string') {
+    offsetUtc = Number(offsetUtc);
+  }
+  let endFix = ' GMT+0' + (offsetUtc / 60 / 60 * 100);
+  return new Date(datestr + endFix);
+}
+
 module.exports = timezone;
